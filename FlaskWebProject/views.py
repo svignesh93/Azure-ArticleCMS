7f7f5339815e5fc5299766107ae3452ba9dbc601
svignesh93 <vignesh.vic727@gmail.com>
2022-08-02 00:12:56 +0530

    Updated azure msal config

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -85,8 +85,11 @@ def authorized():
         return render_template("auth_error.html", result=request.args)
     if request.args.get('code'):
         cache = _load_cache()
-        # TODO: Acquire a token from a built msal app, along with the appropriate redirect URI
-        result = None
+        result = _build_msal_app(cache = cache).acquire_token_by_authorization_code(
+            code = request.args['code'],
+            scopes = Config.SCOPE,
+            redirect_uri = url_for('authorized', _external = True, _scheme = 'https')
+        )
         if "error" in result:
             return render_template("auth_error.html", result=result)
         session["user"] = result.get("id_token_claims")
@@ -111,18 +114,26 @@ def logout():
     return redirect(url_for('login'))
 
 def _load_cache():
-    # TODO: Load the cache from `msal`, if it exists
-    cache = None
+    cache = msal.SerializableTokenCache()
+    if session.get('token_cache'):
+        cache.deserialize(session['token_cache'])
     return cache
 
 def _save_cache(cache):
-    # TODO: Save the cache, if it has changed
-    pass
+    if cache.has_state_changed:
+        session['token_cache'] = cache.serialize()
 
 def _build_msal_app(cache=None, authority=None):
-    # TODO: Return a ConfidentialClientApplication
-    return None
+    return msal.ConfidentialClientApplication(
+        client_id = Config.CLIENT_ID,
+        client_credential = Config.CLIENT_SECRET,
+        authority = authority or Config.AUTHORITY,
+        token_cache = cache
+    )
 
 def _build_auth_url(authority=None, scopes=None, state=None):
-    # TODO: Return the full Auth Request URL with appropriate Redirect URI
-    return None
+    return _build_msal_app(authority = authority).get_authorization_request_url(
+        scopes = scopes or [],
+        state = state or str(uuid.uuid4()),
+        redirect_uri = url_for('authorized', _external = True, _scheme = 'https')
+    )
